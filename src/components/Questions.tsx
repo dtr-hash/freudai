b463a7260d77fd1bfd6bb5cfbc6a052b7af2e3a0
Lasse Kabell <lkabell@users.noreply.github.com>
2025-02-26 16:13:32 +0100

    images

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import data from "../../data.json";
+
+type Option = {
+  label: string;
+  values: string[];
+};
+
+type Question = {
+  path: string;
+  options: Option[];
+};
+
+type Data = {
+  questions: Question[];
+  optionsLabels: string[];
+};
 
 const Questions = () => {
-  console.log("In about...");
   const [count, setCount] = useState(0);
+  const questions: Data = data;
+  const currentQuestion = questions.questions[count];
 
   return (
     <>
       <div>Questions page {count}</div>
-
+      <img 
+        src={`/${currentQuestion.path}`}  
+        alt="Personality test image"
+        className="w-full max-w-xl rounded-lg shadow-lg my-4"
+      />
       <br />
       <Button onClick={() => setCount((count) => count + 1)}>
         Next question
